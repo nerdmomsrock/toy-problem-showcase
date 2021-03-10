7c81b4a065dcb083d1e8306a47f0c6a67d71996b
Ryan Morgan <nerdmomsrock@gmail.com>
2021-03-09 22:53:45 -0500

    conitue toy problem work

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 
 
 class FilterString extends Component {
+    constructor () {
+        super();
+        this.state = {
+            unfilteredArray: [],
+            userInput: '',
+            filteredArray: [],
+
+        }
+    }
+    handleChange = (val) => {
+        this.setState({userInput: val});
+    }
+
+    filteredUpdate = (userInput) => {
+        let family = [...this.state.unfilteredArray];
+        let filteredArray = [];
+        for (let i = 0; i < family.length; i ++){
+            if (family[i].startsWith('C')){
+                this.state.filteredArray.push(family[i])
+            }
+        }
+        this.setState({filteredArray: family});
+    }
     render () {
-        console.log(this.state);
+
+        
         return (
-            <p>FilterString Component</p>
+            <div className="puzzleBox filterStringPB">
+               <h4>Filter String</h4>
+               <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}/>
+               <button className="confirmationButton" onClick={(e) => this.filteredUpdate(this.state.userInput)}>Click To Filter</button>
+               <span className="puzzleText">Unfiltered Array: {JSON.stringify(this.state.unfilteredArray)}</span>
+               <span className="resultsBox filterStringRB">Filtered Array: {JSON.stringify(this.state.filteredArray)}</span>
+
+           </div>
     
            
         )
